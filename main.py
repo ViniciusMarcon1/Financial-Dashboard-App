e939f88fb9c51f0dfa8d61aaa27c3ca2ff13c110
viniciusmarcon1 <vinimarcon5@gmail.com>
2025-05-26 14:06:47 -0300

    Add file uploader, tabs in financial dashboard

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import streamlit as st
-import pandas as pd 
-import plotly as pt 
-import financial_dashboard, ask_ai
 
-main_page = st.Page("loading_info.py", title="Main Page", icon="💾")
-financial_page = st.Page("financial_dashboard.py", title="Page 2", icon="💵")
-ai_page = st.Page("ask_ai.py", title="Page 3", icon="🤖")
+st.set_page_config(page_title='Personal Financial Dashboard', page_icon='🤑', layout='wide')
 
-pg = st.navigation([main_page, financial_page, ai_page])
+loading_info = st.Page("loading_info.py", title="Loading", icon="💾")
+financial_page = st.Page("financial_dashboard.py", title="Dashboard", icon="💵")
+ai_page = st.Page("ask_ai.py", title="Ask AI", icon="🤖")
+
+pg = st.navigation([loading_info, financial_page, ai_page])
 
 # Run the selected page
 pg.run()
